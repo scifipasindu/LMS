c9ea56d6298b6279dd4721901d10c6d7d31b947d
scifipasindu <pasindulakshanperera2019@gmail.com>
2026-01-28 19:22:58 +0530

    Revert output dir to default, remove yarn.lock, bump v0.0.5

--- quasar-project/src/layouts/DashboardLayout.vue
+++ quasar-project/src/layouts/DashboardLayout.vue
@@ -181,7 +181,7 @@
       </div>
       
       <div class="absolute-bottom q-pa-md text-center text-caption text-negative text-weight-bold">
-         v0.0.4 (Config Fix)
+         v0.0.5 (Reverted Config)
       </div>
       
 
